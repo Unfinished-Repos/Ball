5b06e530aa135581df308d27fb8ecd8c5ae5ebb4
Andrew Friedman <andrewfriedman01@gmail.com>
2023-01-02 23:55:48 -0500

    Add content

---
+++ Assets/Script/CameraControll.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var target : Transform;
+var distance = -15;
+var lift = 15;
+function Update () 
+{
+	transform.position = target.position + Vector3(0, lift, distance);
+	transform.LookAt (target);
+}
---
+++ Assets/Script/ControlBall.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var jumpHeight = 8;
+var moveSpeed = 1f;
+var target : Transform;
+private var isFalling = false;
+
+function Update () 
+{
+	//Handle ball rotation
+	transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);		
+	
+	if (Input.GetKeyDown (KeyCode.Space) && isFalling == false){
+		GetComponent.<Rigidbody>().velocity.y = jumpHeight;
+	}
+	isFalling = true;
+}
+
+function OnCollisionStay (){
+	isFalling = false;
+}
